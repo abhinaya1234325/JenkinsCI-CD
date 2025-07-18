73b58e6b3431b0a2ea7a280d19857d43d2fe5b09
abhinaya <palaparthiabhinaya@gmail.com>
2025-07-18 14:42:08 +0530

    Added Addition.java program

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 25;
+        int sum = Integer.sum(num1, num2);
+        System.out.println("The sum using Integer.sum() is: " + sum); // Output: The sum using Integer.sum() is: 40
+    }
+}
